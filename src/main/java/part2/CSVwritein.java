3473a0c5e7451ca7d4cfcc3d5e16dfa0f46bfd19
kiwi <41132322+whs146@users.noreply.github.com>
2023-02-06 00:36:59 -0800

    Update info

--- src/main/java/part2/CSVwritein.java
+++ src/main/java/part2/CSVwritein.java
@@ -1,14 +1,12 @@
 package part2;
 
-import com.opencsv.CSVReader;
+
 import com.opencsv.CSVWriter;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.concurrent.atomic.AtomicInteger;
+
 
 public class CSVwritein {
 
