87e3911ed9b4b628c3f80b28ec1c0ed833855d7b
kiwi <41132322+whs146@users.noreply.github.com>
2023-03-22 22:16:12 -0700

    update clients for hw3

--- src/main/java/part1/MultithreadClient1.java
+++ src/main/java/part1/MultithreadClient1.java
@@ -1,9 +1,13 @@
 package part1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+
 
 
 public class MultithreadClient1 {
@@ -11,6 +15,10 @@ public class MultithreadClient1 {
     final static private int TOTALREQUESTS = 500000;
     private AtomicInteger successCount = new AtomicInteger(0);
     private AtomicInteger failCount = new AtomicInteger(0);
+
+    static List<Long> getLatencies = Collections.synchronizedList(new ArrayList<Long>());
+//    static List<Long> getLatencies1 = Collections.synchronizedList(new ArrayList<Long>());
+
 
 
     public AtomicInteger getFailCount() {
@@ -20,6 +28,7 @@ public class MultithreadClient1 {
     public AtomicInteger getSuccessCount() {
         return successCount;
     }
+
 
 
     public static void main(String[] args) throws InterruptedException {
@@ -41,19 +50,60 @@ public class MultithreadClient1 {
                         }
                     }
 
-
             );
-
         }
+        new Thread(() -> {
+//            when the latch count reaches 0, stop the get thread
+            while(latch.getCount() != 0){
+                for(int i = 0; i < 5; i++){
+                    long startTime = System.currentTimeMillis();
+                    //mix up the two get request
+                    if(i % 2 == 0){
+                        SingleClient1.getMatchRequest();
+                    }
+                    else{
+                        SingleClient1.getMatchStatsRequest();
+                    }
+                    long latency = System.currentTimeMillis() - startTime;
+                    //add the get latency to the synchronizedList
+                    getLatencies.add(latency);
+
+
+                }
+
+                try {
+                    Thread.sleep(850);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }).start();
+
+
+
+
         latch.await();
+
         long end = System.currentTimeMillis();
         pool.shutdown();
+
+
         System.out.println("The number of successful requests sent is " + obj.getSuccessCount());
         System.out.println("The number of unsuccessful requests sent is " + obj.getFailCount());
         System.out.println("The total run time for all threads to complete is " + (end - start) + " milliseconds");
         System.out.println("The number of thread is " + NUMTHREADS);
         System.out.println("The total throughput in requests per second is " + (TOTALREQUESTS / ((end - start) / 1000.0)));
         System.out.println("Little’s Law throughput predictions in requests per second is " + (NUMTHREADS / 0.015));
+        System.out.println("The number of get requests are " + getLatencies.size());
+        System.out.println("The min latency of the get requests in millisecond is " + getLatencies.stream().min(Long::compare).get());
+        System.out.println("The max latency of the get requests in millisecond is " + getLatencies.stream().max(Long::compare).get());
+        System.out.println("The mean latency of the get requests in millisecond is " + getLatencies.stream().mapToLong(Long::longValue).average().getAsDouble());
+        System.out.println("The mean latency of the post requests in millisecond is " + SingleClient1.postLatencies.stream().mapToLong(Long::longValue).average().getAsDouble());
+
+
+
+
 
 
     }
--- src/main/java/part1/SingleClient1.java
+++ src/main/java/part1/SingleClient1.java
@@ -3,20 +3,50 @@ package part1;
 
 import io.swagger.client.ApiClient;
 import io.swagger.client.ApiException;
+import io.swagger.client.api.MatchesApi;
+import io.swagger.client.api.StatsApi;
 import io.swagger.client.api.SwipeApi;
+import io.swagger.client.model.MatchStats;
+import io.swagger.client.model.Matches;
 import org.apache.commons.lang3.RandomStringUtils;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SingleClient1 {
-    private static String url = "http://35.90.21.151:8080/twinder/swipe";
+    private static String url_post = "http://new-101717130.us-west-2.elb.amazonaws.com/twinder/swipe";
+    private static String url_match = "http://new-101717130.us-west-2.elb.amazonaws.com/twinder/matches/";
+    private static String url_stats = "http://new-101717130.us-west-2.elb.amazonaws.com/twinder/stats/";
+
+static List<Long> postLatencies = Collections.synchronizedList(new ArrayList<Long>());
+
+
+    public static void main(String[] args) {
+        List<Long> l = new ArrayList<Long>();
+        l.add(0l);
+        l.add(1l);
+        l.add(3l);
+        l.add(5l);
+        l.add(2l);
+
+        System.out.println(l.stream().max(Long::compare).get());
+        System.out.println(l.stream().min(Long::compare).get());
+        System.out.println(l.stream().count());
+        System.out.println(l.stream().mapToLong(Long::longValue).average().getAsDouble());
+
+
+
+
+    }
 
     public static void run(MultithreadClient1 obj) throws RuntimeException{
         SwipeApi swipeApi = new SwipeApi();
         io.swagger.client.model.SwipeDetails body= new io.swagger.client.model.SwipeDetails();
         ApiClient apiClient = swipeApi.getApiClient();
         String[] strs = generateRandom();
-        apiClient.setBasePath(url);
+        apiClient.setBasePath(url_post);
         body.setSwiper(strs[1]);
         body.setSwipee(strs[2]);
         body.setComment(strs[3]);
@@ -32,6 +62,7 @@ public class SingleClient1 {
             //record successful and unsuccessful request counts
             if(result/100 == 2) obj.getSuccessCount().getAndIncrement();
             else obj.getFailCount().getAndIncrement();
+//            System.out.println(result);
 
 
         } catch (ApiException e) {
@@ -54,5 +85,49 @@ public class SingleClient1 {
         return str;
 
     }
+    /*
+       Send a getMatchRequest request
+     */
+    public static void getMatchRequest(){
 
+        MatchesApi apiInstance = new MatchesApi();
+        ApiClient apiClient = apiInstance.getApiClient();
+        String userID = generateRandom()[1];
+        apiClient.setBasePath(url_match+userID);
+        try {
+            long start = System.currentTimeMillis();
+            Matches result = apiInstance.matches(userID);
+            //add the post latencies to the list
+            postLatencies.add(System.currentTimeMillis() - start);
+//            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling MatchesApi#matches");
+            e.printStackTrace();
+        }
+
+    }
+    /*
+       Send a getMatchStats request
+     */
+    public static void getMatchStatsRequest() {
+
+
+        StatsApi statsApi = new StatsApi();
+        ApiClient apiClient1 = statsApi.getApiClient();
+
+        String userID = generateRandom()[1];
+        apiClient1.setBasePath(url_stats + userID);
+        try {
+            long start = System.currentTimeMillis();
+            MatchStats matchStats = statsApi.matchStats(userID);
+            //add the post latencies to the list
+            postLatencies.add(System.currentTimeMillis() - start);
+//            System.out.println(matchStats);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling StatsApi#matchStats");
+            e.printStackTrace();
+        }
+
+
+    }
 }
